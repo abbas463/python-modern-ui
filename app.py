6d7778d834e034b681d473c7698d2bd11bb4756e
Abbas463 <abbashilal463@gmail.com>
2026-02-23 10:29:23 +0430

    Lable and Frame

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("dark-blue")
+
+root = customtkinter.CTk()
+root.geometry("500x350")
+
+def login():
+    print("Test")
+
+frame = customtkinter.CTkFrame(master=root)
+frame.pack(pady=20, padx=60, fill="both", expand=True)
+
+label = customtkinter.CTkLabel(master=frame, text="Login", font=("Roboto", 24))
+label.pack(pady=12, padx=10)
